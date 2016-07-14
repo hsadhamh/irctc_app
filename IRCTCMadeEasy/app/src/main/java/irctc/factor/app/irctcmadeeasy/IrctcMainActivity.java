2be8cf9b194784dd2a3958b4f61b6073d3811d7d
shussain <hsadham@live.com>
2016-07-14 14:53:06 +0530

    Web View changes

--- IRCTCMadeEasy/app/src/main/java/irctc/factor/app/irctcmadeeasy/IrctcMainActivity.java
+++ IRCTCMadeEasy/app/src/main/java/irctc/factor/app/irctcmadeeasy/IrctcMainActivity.java
@@ -8,10 +8,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
+
+import com.rey.material.widget.ProgressView;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import irctc.factor.app.irctcmadeeasy.WebView.LxWebView;
 
@@ -52,9 +58,7 @@ public class IrctcMainActivity extends AppCompatActivity {
             "         \t\tconsole.log('Click event done');\n" +
             "         \t\t$(this).focus(); \n" +
             "         \t});\n" +
-            "\n" +
-            "         $(\"a[id=refresh]\").click(function(e){ $(\"input[name='j_captcha']\").trigger('click'); });\n" +
-            "         $(\"a[id=refresh]\").trigger('click');\n" +
+            "\t\t$(\"input[name='j_captcha']\").trigger('click');\n" +
             "     }\n" +
             " }\n" +
             "\n" +
@@ -66,10 +70,19 @@ public class IrctcMainActivity extends AppCompatActivity {
             "         if (isLoginPage()) {\n" +
             "             loginToForm();\n" +
             "             $(\"img[id='cimage']\").width('125%');\n" +
+            "\n" +
+            "             scrollToGivenId($(\"input[id='usernameId']\"));\n" +
             "         } else {\n" +
             "             window.location.href = \"https://www.irctc.co.in/eticketing/loginHome.jsf\";\n" +
             "         }\n" +
             "     } \n" +
+            " }\n" +
+            "\n" +
+            " function scrollToGivenId(wrapper)\n" +
+            " {\n" +
+            " \t$('html, body').animate({\n" +
+            "    \tscrollTop: wrapper.offset().top\n" +
+            "\t}, 1000);\n" +
             " }\n" +
             "\n" +
             " doAutomation();";
@@ -79,16 +92,30 @@ public class IrctcMainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_irctc_main);
 
         final WebView webView = (WebView) findViewById(R.id.webView);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setDomStorageEnabled(true);
+        final ProgressView pv_linear_determinate = (ProgressView) findViewById(R.id.progress_pv_linear_determinate);
 
-        //webView.getSettings().setDefaultZoom(WebSettings.ZoomDensity.FAR);
-        //  http://stackoverflow.com/questions/21552912/android-web-view-inject-local-javascript-file-to-remote-webpage
+        webView.getSettings().setUserAgentString(
+                "Mozilla/5.0 (Windows NT 6.1; rv:13.0) Gecko/20100101 Firefox/12");
+
         assert webView != null;
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress)
+            {
+                pv_linear_determinate.setProgress(pv_linear_determinate.getProgress() + progress + 0.1f);
+                //Make the bar disappear after URL is loaded, and changes string to Loading...
+                // Return the app name after finish loading
+                if(progress == 100) {
+                    pv_linear_determinate.stop();
+                    pv_linear_determinate.setVisibility(View.GONE);
+                }
+            }
+        });
+
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
-                Toast.makeText(getApplicationContext(), "Load Finsihsed", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Load Finished", Toast.LENGTH_SHORT).show();
                 if (Build.VERSION.SDK_INT >= 19) {
                     webView.requestFocus(View.FOCUS_DOWN);
                     view.evaluateJavascript(mJsForTicket, new ValueCallback<String>() {
@@ -98,10 +125,14 @@ public class IrctcMainActivity extends AppCompatActivity {
                         }
                     });
 
-                    //webView.setInitialScale(1);
+                    String webUrl = webView.getUrl();
+                    String re1=".*?(\\/www\\.irctc\\.co\\.in\\/eticketing\\/loginHome\\.jsf)";
 
-                    //InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                    //mgr.showSoftInput(webView, InputMethodManager.SHOW_IMPLICIT);
+                    if(isLoginPage(webUrl, re1)) {
+                        Toast.makeText(getApplicationContext(), "Show input ", Toast.LENGTH_SHORT).show();
+                        InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                        mgr.showSoftInput(webView, InputMethodManager.SHOW_IMPLICIT);
+                    }
                 } else {
                     view.loadUrl(mJsForTicket);
                 }
@@ -111,7 +142,14 @@ public class IrctcMainActivity extends AppCompatActivity {
             }
         });
 
+        pv_linear_determinate.setProgress(0f);
+        pv_linear_determinate.start();
         webView.loadUrl("http://www.irctc.co.in/");
+    }
 
+    public boolean isLoginPage(String url, String pattern){
+        Pattern p = Pattern.compile(pattern,Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+        Matcher m = p.matcher(url);
+        return m.find();
     }
 }
