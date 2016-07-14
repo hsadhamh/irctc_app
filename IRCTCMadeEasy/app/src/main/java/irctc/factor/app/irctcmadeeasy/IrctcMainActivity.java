e06591c15d37852074d911858f4e4a1bd9b151b2
shussain <hsadham@live.com>
2016-07-14 19:28:49 +0530

    Web View changes commit

--- IRCTCMadeEasy/app/src/main/java/irctc/factor/app/irctcmadeeasy/IrctcMainActivity.java
+++ IRCTCMadeEasy/app/src/main/java/irctc/factor/app/irctcmadeeasy/IrctcMainActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -18,6 +19,8 @@ import android.widget.Toast;
 
 import com.rey.material.widget.ProgressView;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -28,7 +31,13 @@ import irctc.factor.app.irctcmadeeasy.WebView.LxWebView;
 public class IrctcMainActivity extends AppCompatActivity {
 
     String mJsAutoFile = TicketConstants.JavaScriptForAutoFill;
+
     String mLoginUrlPattern =".*?(\\/www\\.irctc\\.co\\.in\\/eticketing\\/loginHome\\.jsf)";
+    String mPassengerListPage = ".*?(\\/www\\.irctc\\.co\\.in\\/eticketing\\/trainbetweenstns\\.jsf";
+    String mGoogleAdsPattern =".*?(pagead2\\.googlesyndication\\.com)";
+    String mGoogleAdContainerPattern=".*?(tpc\\.googlesyndication\\.com)";
+    String mGoogleAdsPattern2 = ".*?(securepubads\\.g\\.doubleclick\\.net\\/)";	;
+
     String mHomeURL = "http://www.irctc.co.in/eticketing/loginHome.jsf";
 
     @BindView(R.id.irctc_web_view_id)
@@ -54,11 +63,17 @@ public class IrctcMainActivity extends AppCompatActivity {
         mWebIrctc.loadUrl(mHomeURL);
     }
 
-    public boolean isLoginPage(String url){
-        Pattern p = Pattern.compile(mLoginUrlPattern, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
-        Matcher m = p.matcher(url);
+    public boolean isLoginPage(String url){ return isMatch(url, mLoginUrlPattern); }
+
+    public boolean isPassengerListPage(String url){ return isMatch(url, mPassengerListPage); }
+
+    public boolean isMatch(String text, String pattern){
+        Pattern p = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+        Matcher m = p.matcher(text);
         return m.find();
     }
+
+    public boolean isGoogleAds(String url){ return (isMatch(url, mGoogleAdsPattern) || isMatch(url, mGoogleAdContainerPattern) || isMatch(url, mGoogleAdsPattern2)); }
 
     public void setChromeClientProgress(){
         mWebIrctc.setWebChromeClient(new WebChromeClient() {
@@ -79,31 +94,34 @@ public class IrctcMainActivity extends AppCompatActivity {
 
     public void setWebViewClient() {
         mWebIrctc.setWebViewClient(new WebViewClient() {
-            /*@Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                Toast.makeText(getApplicationContext(), "Load Url Override", Toast.LENGTH_SHORT).show();
-                mProgress.setProgress(0.0f);
-                mProgress.start();
-                mProgress.setVisibility(View.VISIBLE);
 
+            @Override
+            public WebResourceResponse shouldInterceptRequest (WebView view, String url){
+                return super.shouldInterceptRequest(view, url);
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 view.loadUrl(url);
                 return true;
-            }*/
+            }
+
+            @Override
+            public void onLoadResource(WebView view, String url) {
+                super.onLoadResource(view, url);
+            }
 
             @Override
             public void onPageFinished(WebView view, String url) {
-                Toast.makeText(getApplicationContext(), "Load Finished", Toast.LENGTH_SHORT).show();
                 if (Build.VERSION.SDK_INT >= 19) {
                     mWebIrctc.requestFocus(View.FOCUS_DOWN);
                     view.evaluateJavascript(mJsAutoFile, new ValueCallback<String>() {
                         @Override
-                        public void onReceiveValue(String s) {
-                            Toast.makeText(getApplicationContext(), "on receive - eval script.", Toast.LENGTH_SHORT).show();
-                        }
+                        public void onReceiveValue(String s) {}
                     });
 
                     String webUrl = mWebIrctc.getUrl();
-                    if(isLoginPage(webUrl)) {
+                    if(isLoginPage(webUrl) || isPassengerListPage(webUrl)) {
                         showSoftKeyBoard();
                     }
                 } else {
@@ -111,7 +129,8 @@ public class IrctcMainActivity extends AppCompatActivity {
                 }
             }
             public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-                Toast.makeText(getApplicationContext(), description, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),
+                        "URL : " + failingUrl +"; ErrorCode : " + errorCode + "; Description : " + description, Toast.LENGTH_SHORT).show();
             }
         });
 
